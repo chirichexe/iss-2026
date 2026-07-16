4dd88071401ac9ca456f4bf67404f47ac03d03ab
Davide Chirichella <davidechirichella10@gmail.com>
2026-07-16 02:56:59 +0200

    update: sprint2 esp32 code

--- sprint2/prototype/esp32/main.py
+++ sprint2/prototype/esp32/main.py
@@ -1,27 +1,30 @@
 import machine
-import network
 import time
+import network
 from umqtt.simple import MQTTClient
 from wifi_config import MQTT_BROKER, PASSWORD, SSID
 
-# Configurazione applicativa
-CLIENT_ID = 'esp32_sonar'
+# config
+CLIENT_ID = 'esp32_sonar_node'
 TOPIC_PUB = b'sensore/sonar'
 TOPIC_SUB = b'comando/led'
 
-# Pin
+# pins
 trigger = machine.Pin(26, machine.Pin.OUT)
 echo = machine.Pin(25, machine.Pin.IN)
-led = machine.Pin(2, machine.Pin.OUT) # LED integrato
+led = machine.Pin(2, machine.Pin.OUT)
 
 def sub_cb(topic, msg):
-    print("Messaggio ricevuto:", msg)
     if msg == b'blink':
         for _ in range(5):
             led.value(1)
             time.sleep(0.2)
             led.value(0)
             time.sleep(0.2)
+    elif msg == b'on':
+        led.value(1)
+    elif msg == b'off':
+        led.value(0)
 
 def connect_wifi():
     wlan = network.WLAN(network.STA_IF)
@@ -31,31 +34,39 @@ def connect_wifi():
         while not wlan.isconnected():
             time.sleep(1)
 
-# Inizializzazione
-connect_wifi()
-client = MQTTClient(CLIENT_ID, MQTT_BROKER)
-client.set_callback(sub_cb)
-client.connect()
-client.subscribe(TOPIC_SUB)
-print("MQTT Connesso e in ascolto su", TOPIC_SUB)
+def connect_mqtt():
+    client = MQTTClient(CLIENT_ID, MQTT_BROKER)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(TOPIC_SUB)
+    return client
 
-try:
-    while True:
-        # Controlla messaggi in arrivo (non bloccante)
+#init
+connect_wifi()
+client = connect_mqtt()
+
+# main loop
+while True:
+    try:
         client.check_msg()
         
-        # Logica sonar
         trigger.value(0)
         time.sleep_us(5)
         trigger.value(1)
         time.sleep_us(10)
         trigger.value(0)
+        
         duration = machine.time_pulse_us(echo, 1, 30000)
         dist = (duration * 0.0343) / 2 if duration >= 0 else 0
         
+        # publish data
         client.publish(TOPIC_PUB, str(dist))
-        print("Distanza:", dist)
         
-        time.sleep(1)
-except Exception as e:
-    print("Errore:", e)
+        time.sleep(0.5)
+        
+    except Exception:
+        try:
+            time.sleep(2)
+            client = connect_mqtt()
+        except:
+            pass
